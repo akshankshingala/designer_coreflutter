d9ece78e0b0643d78a57333ee3b079565a40f0c5
Akshank... <singalaakshank98@gmail.com>
2024-02-17 09:34:30 +0530

    designe...r

---
+++ android/app/src/main/kotlin/com/example/flutter_application_desiger/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_application_desiger
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "list Of Fruits",
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Color(0xff009689),
+          title: Text(
+            "🛍  List of Fruist",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        body: Center(
+          child: RichText(
+            text: const TextSpan(
+              text: "🍎 A p p l e\n\n",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold, color: Colors.red, fontSize: 30),
+              children: [
+                TextSpan(
+                  text: "🍇  G r e p s\n\n",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFe040fe),
+                      fontSize: 30),
+                ),
+                TextSpan(
+                  text: "🍒  c h e r r y\n\n",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF9c27b1),
+                      fontSize: 30),
+                ),
+                TextSpan(
+                  text: "🍓  s t r a w b e r r y\n\n",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFe91e63),
+                      fontSize: 30),
+                ),
+                TextSpan(
+                  text: "🥭  m a n g o\n\n",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFff9800),
+                      fontSize: 30),
+                ),
+                TextSpan(
+                  text: "🍍  p i n e a p p l e\n\n",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF4caf50),
+                      fontSize: 30),
+                ),
+                TextSpan(
+                  text: "🍋  L e m o n\n\n",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFffc107),
+                      fontSize: 30),
+                ),
+                TextSpan(
+                  text: "🍉  W a t e r m e l o n e\n\n",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF8bc34a),
+                      fontSize: 30),
+                ),
+                TextSpan(
+                  text: "🥥  C o c o n u t\n\n",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF795548),
+                      fontSize: 30),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
